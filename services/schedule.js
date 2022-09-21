9c33a3d68aa77bf8dc588100db811ba5dd293c17
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-09-22 03:00:48 +0800

    resolve conflicts

--- services/schedule.js
+++ services/schedule.js
@@ -7,30 +7,40 @@ const Task = require("../models/taskModel");
 const taskModel = require("../models/taskModel");
 
 const scheduledTime = (task) => {
-  const day = new Date(task.start.on).getDay();
-  const hours =
-    task.time !== "allDay"
-      ? new Date(`${task.start.on} ${task.time}`).getHours()
-      : 0;
-  const minutes =
-    task.time !== "allDay"
-      ? new Date(`${task.start.on} ${task.time}`).getMinutes()
-      : 0;
-  const id = task._id;
+  const day = new Date(task.next).getDay();
+  const date = new Date(task.next).getDate();
+  const hours = task.time !== "allDay" ? new Date(task.next).getHours() : 0;
+  const minutes = task.time !== "allDay" ? new Date(task.next).getMinutes() : 0;
 
   let cron;
+  let rule;
 
   if (task.frequency === "once") {
-    cron = ``;
+    rule = new Date(task.next);
   } else if (task.frequency === "daily") {
     cron = `${minutes} ${hours} * * *`;
   } else if (task.frequency === "weekly") {
     cron = `${minutes} ${hours} * * ${day}`;
   } else if (task.frequency === "monthly") {
-    cron = `${minutes} ${hours} ${day} * *`;
+    cron = `${minutes} ${hours} ${date} * *`;
   }
 
-  return cron ? cron : new Date(`${task.start.on} ${task.time}`);
+  if (cron && task.end.by !== "date") {
+    rule = {
+      start: new Date(task.next),
+      end: new Date(999999999999999),
+      rule: cron,
+    };
+  }
+  if (cron && task.end.by === "date") {
+    rule = {
+      start: new Date(task.next),
+      end: new Date(`${task.end.on}`),
+      rule: cron,
+    };
+  }
+
+  return rule;
 };
 
 const updateTask = async (payload, task) => {
@@ -45,65 +55,73 @@ const updateTask = async (payload, task) => {
 };
 
 const scheduledJob = async (task) => {
-  const users = await User.find({});
-  const chunks = _.chunk(users, 500);
-  const promises = chunks.map(async (chunk) => {
-    const tokens = [];
-    chunk.forEach((user) => {
-      if (user.fcmToken) {
-        tokens.push(user.fcmToken);
-      }
-    });
-    const payload = {
-      tokens,
-      title: task.name,
-      body: task.description ? task.description : "",
-    };
-    return firebaseAdmin
-      .sendMulticastNotification(payload)
-      .then((response) => console.log(response));
-  });
-  await Promise.all(promises);
+  console.log(scheduledTime(task));
+  const job = schedule.scheduleJob(
+    task._id.toString(),
+    scheduledTime(task),
+    async () => {
+      const users = await User.find({});
+      const chunks = _.chunk(users, 500);
+      const promises = chunks.map(async (chunk) => {
+        const tokens = [];
+        chunk.forEach((user) => {
+          if (user.fcmToken) {
+            tokens.push(user.fcmToken);
+          }
+        });
+        const payload = {
+          tokens,
+          title: task.name,
+          body: task.description ? task.description : "",
+        };
+        return firebaseAdmin
+          .sendMulticastNotification(payload)
+          .then((response) => console.log(response));
+      });
+      await Promise.all(promises);
 
-  //update occurence of task
-  updateTask(
-    {
-      occurrence: (task.occurrence += 1),
-    },
-    task
-  );
+      console.log("next", new Date(job.nextInvocation()));
 
-  //update status of task
-  if (task.frequency === "once") {
-    updateTask(
-      {
-        status: "finished",
-      },
-      task
-    );
-    schedule.cancelJob(task._id.toString());
-    console.log("task finished");
-  }
-
-  if (task.end.by === "occurrence") {
-    if (task.end.on === task.occurrence) {
+      //update occurence of task
       updateTask(
         {
-          status: "finished",
+          occurrence: (task.occurrence += 1),
+          next: new Date(job.nextInvocation()),
         },
         task
       );
-      schedule.cancelJob(task._id.toString());
-      console.log("task finished");
+
+      //update status of task
+      if (task.frequency === "once") {
+        updateTask(
+          {
+            status: "finished",
+          },
+          task
+        );
+        schedule.cancelJob(task._id.toString());
+        console.log("task finished");
+      }
+
+      if (task.end.by === "occurrence") {
+        if (task.end.on === task.occurrence) {
+          updateTask(
+            {
+              status: "finished",
+            },
+            task
+          );
+          schedule.cancelJob(task._id.toString());
+          console.log("task finished");
+        }
+      }
     }
-  }
+  );
 };
 
 const createSchedule = async (task) => {
   try {
-    schedule.scheduleJob(task._id.toString(), scheduledTime(task), async () => {
-      scheduledJob(task);
-    });
+    scheduledJob(task);
   } catch (error) {
     throw error;
   }
@@ -115,16 +133,10 @@ const reSchedule = async () => {
 
     tasks.forEach((task) => {
       if (task.status === "ongoing") {
-        schedule.scheduleJob(
-          task._id.toString(),
-          scheduledTime(task),
-          async () => {
-            scheduledJob(task);
-          }
-        );
+        scheduledJob(task);
       }
       if (task.end.by === "date") {
-        scheduler.scheduleCancelSchedule(new Date(task.end.on), task._id);
+        scheduler.scheduleCancelJob(new Date(task.end.on), task._id);
       }
     });
     console.log("tasks rescheduled");
@@ -133,11 +145,17 @@ const reSchedule = async () => {
   }
 };
 
-const scheduleCancelSchedule = async (scheduledCancel, task) => {
+const scheduleCancelJob = async (scheduledCancel, task) => {
   schedule.scheduleJob(scheduledCancel, () => {
     schedule.cancelJob(task);
   });
 };
+
+/* const scheduleStartJob = async (scheduledStart, task) => {
+  schedule.scheduleJob(scheduledStart, () => {
+    schedule.cancelJob(task);
+  });
+}; */
 
 const getJobs = () => {
   return schedule.scheduledJobs;
@@ -147,7 +165,7 @@ const scheduler = {
   createSchedule,
   reSchedule,
   getJobs,
-  scheduleCancelSchedule,
+  scheduleCancelJob,
 };
 
 module.exports = scheduler;
