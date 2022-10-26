ecfebe07d965caa8e8c081b5de3d4d2384cb2dfa
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-10-26 21:49:08 +0800

    debug time

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ appServer.listen(port, () => {
 
 const date = new Date("2022-11-11");
 console.log("debug", new Date("10-26-2022 8:00").toString());
+console.log("debug", new Date("10-26-2022 8:00 GMT+0800").toString());
 console.log(
   "debug",
   new Date(
